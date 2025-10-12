f626acb726ba6b020b83ddcdfdb35bfd2519109d
Skinatro <work.omkate@gmail.com>
2025-10-12 21:41:31 +0530

    Resolve stash pop conflicts preferring stashed changes

--- Addons/brackeys-proto-controller-main/proto_controller/door_ray_cast_3d.gd
+++
@@ -1,7 +0,0 @@
-extends RayCast3D
-
-func _process(delta: float) -> void:
-	if is_colliding():
-		var hitObj = get_collider()
-		if hitObj.has_method("interact") && Input.is_action_just_pressed("interact"):
-			hitObj.interact()
