1798015717853c1dd0a7ea99ea3ce4bf95c940bc
Sarvesh S. Devalekar <devalekarsarvesh@gmail.com>
2025-10-14 22:16:24 +0530

    2 games integrated :) + minor fixes

--- addons/brackeys-proto-controller-main/proto_controller/proto_controller.gd
+++ addons/brackeys-proto-controller-main/proto_controller/proto_controller.gd
@@ -65,7 +65,7 @@ var ristrictMovement:=false
 
 
 #STAIRS
-const MAX_STEP_HEIGHT = 0.35
+const MAX_STEP_HEIGHT = 0.34
 var _snapped_to_stairs_last_frame:= false
 var _last_frame_was_on_floor= -INF
 
